70e36080cf0159c7a6c687f1b03df28d90e6e5ba
JAdewoye <j.adewoye@outlook.com>
2025-11-11 18:01:52 +0000

    Checkpoint in adding postgreSQL. Need to sort out naming conventions

---
+++ src/TradeDatabase.cpp
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------------
+// Inlcudes
+//----------------------------------------------------------------------------------
+#include "TradeDatabase.h"
+#include <iostream>
+//----------------------------------------------------------------------------------
+// Function Definitions
+//----------------------------------------------------------------------------------
+TradeDatabase::TradeDatabase(const std::string& connection_string) : connection_(std::make_unique<pqxx::connection>(connection_string))
+{
+    if (connection_ && connection_->is_open()) {
+        std::cout << "Connected to database successfully: " << connection_->dbname() << "\n";
+        ensureTableExists();
+    } else {
+        throw std::runtime_error("Failed to connect to database");
+    }
+}
+//----------------------------------------------------------------------------------
---
+++ src/TradeDatabase.h
@@ -0,0 +1,25 @@
+#pragma once
+//----------------------------------------------------------------------------------
+// Inlcudes
+//----------------------------------------------------------------------------------
+#include <string>
+#include <memory>
+#include "TradeQueue.h"
+#include <pqxx/pqxx>
+//----------------------------------------------------------------------------------
+// Macros
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+// Class Definitions
+//----------------------------------------------------------------------------------
+class TradeDatabase {
+public:
+    TradeDatabase(const std::string& connection_string);
+    ~TradeDatabase();
+
+    bool saveTrade(const Trade& trade);
+private:
+    std::unique_ptr<pqxx::connection> connection_;
+    void ensureTableExists();
+};
+
